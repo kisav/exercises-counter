4ebf5ebe3e020d6425decc70bd410b978c8d7a22
Vlad <4vlad.ilyin@gmail.com>
2026-01-19 21:49:08 +0300

    bugfix/removedUnnecessaryMethods

--- upr.py
+++ upr.py
@@ -1,64 +1,9 @@
 import json
 import time as t
 
-try:
-    pushups = int(input('Введите число отжиманий: '))
-
-    if pushups == -1:
-        print('s')
-        exit()
-        print('a')
-    elif pushups == -2:
-        total_squats = []
-        total_pushups = []
-        total_abdominal = []
-        pushups = int(input('Введите число отжиманий: '))
-
-    squats = int(input('Введите число приседаний: '))
-    abdominal = int(input('Введите число пресса: '))
-except Exception as e:
-    print(f'Ошибка: {e}')
-    pushups = int(input('Введите число отжиманий: '))
-    squats = int(input('Введите число приседаний: '))
-    abdominal = int(input('Введите число пресса: '))
-print('проверки прошли 1')
-
-if pushups or squats or abdominal < -2:
-    print('Ошибка входных данных. Попробуйте ещё раз.')
-    pushups = int(input('Введите число отжиманий: '))
-    squats = int(input('Введите число приседаний: '))
-    abdominal = int(input('Введите число пресса: '))
-print('проверки прошли')
-
-try:
-    with open("exersices.json", "r", encoding="utf-8") as f:
-        total_pushups, total_squats, total_abdominal = json.load(f)
-except json.JSONDecodeError:
-    total_squats = []
-    total_pushups = []
-    total_abdominal = []
-print('Opened')
-
-total_squats.append(squats)
-total_pushups.append(pushups)
-total_abdominal.append(abdominal)
-print('added')
-
-while True:
-    t.sleep(3)
+def main():
     try:
         pushups = int(input('Введите число отжиманий: '))
-
-        if pushups == -1:
-            print('s')
-            exit()
-            print('a')
-        elif pushups == -2:
-            total_squats = []
-            total_pushups = []
-            total_abdominal = []
-            pushups = int(input('Введите число отжиманий: '))
-
         squats = int(input('Введите число приседаний: '))
         abdominal = int(input('Введите число пресса: '))
     except Exception as e:
@@ -66,24 +11,52 @@ while True:
         pushups = int(input('Введите число отжиманий: '))
         squats = int(input('Введите число приседаний: '))
         abdominal = int(input('Введите число пресса: '))
-    print('проверки прошли 1')
 
-    if pushups or squats or abdominal < -2:
-        print('Ошибка входных данных. Попробуйте ещё раз.')
-        pushups = int(input('Введите число отжиманий: '))
-        squats = int(input('Введите число приседаний: '))
-        abdominal = int(input('Введите число пресса: '))
-    print('проверки прошли')
+    try:
+        with open("exersices.json", "r", encoding="utf-8") as f:
+            total_pushups, total_squats, total_abdominal = json.load(f)
+    except json.JSONDecodeError:
+        total_squats = []
+        total_pushups = []
+        total_abdominal = []
+    print('Opened')
 
     total_squats.append(squats)
     total_pushups.append(pushups)
     total_abdominal.append(abdominal)
+    print('added')
 
-    with open("exersices.json", "w", encoding="utf-8") as f:
-        json.dump([total_pushups, total_squats, total_abdominal], f)
+    while True:
+        t.sleep(3)
+        try:
+            pushups = int(input('Введите число отжиманий: '))
+            squats = int(input('Введите число приседаний: '))
+            abdominal = int(input('Введите число пресса: '))
+        except Exception as e:
+            print(f'Ошибка: {e}')
+            pushups = int(input('Введите число отжиманий: '))
+            squats = int(input('Введите число приседаний: '))
+            abdominal = int(input('Введите число пресса: '))
+
+        total_squats.append(squats)
+        total_pushups.append(pushups)
+        total_abdominal.append(abdominal)
+
+        sum_pushups = sum(total_pushups)
+        sum_squats = sum(total_squats)
+        sum_abdominal = sum(total_abdominal)
 
 
+        print('Всего:')
+        print(f'Отжиманий: {sum_pushups}')
+        print(f'Приседаний: {sum_squats}')
+        print(f'Пресса: {sum_abdominal}')
 
 
+        with open("exersices.json", "w", encoding="utf-8") as f:
+            json.dump([total_pushups, total_squats, total_abdominal], f)
+
+if __name__ == "__main__":
+    main()
 
 
